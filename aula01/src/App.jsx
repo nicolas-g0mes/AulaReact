9d9bfc80e1b6632843d1f60e9129a892486bdfb9
nicolas-g0mes <niicolasogomess0708@gmail.com>
2026-08-05 10:48:47 -0300

    05/08

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,29 +1,15 @@
+import "./App.css";
+import Exemplo1 from "./Exemplo1.jsx";
+
 export default function App() 
 {
-  let nome, idade, altura;
-
-  nome = "Nicolas";
-  idade = 20;
-  altura = 1.70;
   return (
     <div>
-      <h1>Hello, World!</h1>
 
-      <p style={ {color: 'red'} }>
-        Olá, seja bem vindo ao React com JavaScript
-      </p>
+        <h1>Aula 01 - Primeiros Passos no React</h1>
+        <Exemplo1/>
 
-      <p>
-        Olá <b>{nome}</b> seja bem vindo ao React.
-      </p>
 
-      <p>
-        Sua idade é <span style={ {color: 'blue'} }>{idade} anos</span>, e sua altura é <span style={ {color: 'green'} }>{altura}</span> metros.
-      </p>
-
-      <p>
-        Você já viveu {idade * 365} dias.
-      </p>
     </div>
-  );
+  )
 }
---
+++ aula01/src/Exemplo1.jsx
@@ -0,0 +1,15 @@
+export default function Exemplo1() 
+{
+    return (
+        <div className="conteudo">
+
+            <h3>Exemplo 1</h3>
+
+            <p>
+                Olá seja bem vindo ao seu primeiro componente.
+            </p>
+
+        </div>
+    )
+
+}
